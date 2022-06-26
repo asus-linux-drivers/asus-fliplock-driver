f325c7db1a4ee1078a1ed85cb1267e550c4efdae
Lukáš Drahník <ldrahnik@gmail.com>
2022-06-26 17:36:36 +0200

    Added disable/enable of capslock led

--- conf/up541ea.py
+++ conf/up541ea.py
@@ -1,11 +1,15 @@
 from libevdev import EV_KEY
 
 to_tablet_mode_actions = [
-    "echo 0 | sudo tee -a /sys/class/leds/asus::kbd_backlight/brightness"
+    "cat /sys/class/leds/asus::kbd_backlight/brightness | sudo tee /tmp/kbd_backlight_brightness",
+    "echo 0 | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
+    "cat /sys/class/leds/input3\:\:capslock/brightness | sudo tee /tmp/input3_capslock_brightness",
+    "echo 0 | sudo tee /sys/class/leds/input3\:\:capslock/brightness"
 ]
 
 to_laptop_mode_actions = [
-    "cat /sys/class/leds/asus::kbd_backlight/brightness_hw_changed | sudo tee -a /sys/class/leds/asus::kbd_backlight/brightness"
+    "cat /tmp/kbd_backlight_brightness | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
+    "cat /tmp/input3_capslock_brightness | sudo tee /sys/class/leds/input3\:\:capslock/brightness"
 ]
 
 flip_key = EV_KEY.KEY_PROG2
