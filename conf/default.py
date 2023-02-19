f57a7439831be281991d9ec436ae5e2a986c1db7
Lukáš Drahník <ldrahnik@gmail.com>
2023-02-19 11:47:20 +0100

    Update default.py

--- conf/default.py
+++ conf/default.py
@@ -1,16 +1,20 @@
 from libevdev import EV_KEY
 
 laptop_mode_actions = [
-    # backward setting up saved backlight brightness (only in case inverted was previous state! otherwise tmp file does not exist)
+    # setting up saved keyboard backlight brightness (restore from backup)
     "test -f /tmp/kbd_backlight_brightness && cat /tmp/kbd_backlight_brightness | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
-    # backward setting up saved capslock led (only in case inverted was previous state! otherwise tmp file does not exist)
+    # setting up saved capslock backlight brightness (restore from backup)
     "test -f /tmp/input3_capslock_brightness && cat /tmp/input3_capslock_brightness | sudo tee /sys/class/leds/input3\:\:capslock/brightness"
 ]
 
 tablet_mode_actions = [
+    # save keyboard backlight brightness (do a backup)
     "cat /sys/class/leds/asus::kbd_backlight/brightness | sudo tee /tmp/kbd_backlight_brightness",
+    # disable keyboard backlight brightness
     "echo 0 | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
+    # save capslock led brightness (do a backup)
     "cat /sys/class/leds/input3\:\:capslock/brightness | sudo tee /tmp/input3_capslock_brightness",
+    # disable capslock led brightness
     "echo 0 | sudo tee /sys/class/leds/input3\:\:capslock/brightness"
 ]
 
