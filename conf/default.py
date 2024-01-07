81f777c144de08d18ed5689a73a5b2952b2d5f30
ldrahnik@gmail.com <ldrahnik@gmail.com>
2024-01-07 01:16:34 +0100

    Changed capslock input for current UN5401QAB & fixed triggering only by event key & added camera led by asus_wmi_devs

--- conf/default.py
+++ conf/default.py
@@ -4,9 +4,11 @@ laptop_mode_actions = [
     # setting up saved keyboard backlight brightness (restore from backup)
     "test -f /tmp/kbd_backlight_brightness && cat /tmp/kbd_backlight_brightness | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
     # setting up saved capslock backlight brightness (restore from backup)
-    "test -f /tmp/input3_capslock_brightness && cat /tmp/input3_capslock_brightness | sudo tee /sys/class/leds/input3::capslock/brightness",
+    "test -f /tmp/input2_capslock_brightness && cat /tmp/input2_capslock_brightness | sudo tee /sys/class/leds/input2::capslock/brightness",
     # setting up saved micmute backlight brightness (restore from backup)
     "test -f /tmp/micmute_brightness && cat /tmp/micmute_brightness | sudo tee /sys/class/leds/platform::micmute/brightness",
+    # setting up saved camera backlight brightness (restore from backup)
+    "test -f /tmp/camera_brightness && echo '0x60079' | sudo tee '/sys/kernel/debug/asus-nb-wmi/dev_id' >/dev/null && cat /tmp/camera_brightness | sudo tee '/sys/kernel/debug/asus-nb-wmi/ctrl_param' && sudo cat '/sys/kernel/debug/asus-nb-wmi/devs' >/dev/null"
 ]
 
 tablet_mode_actions = [
@@ -15,13 +17,17 @@ tablet_mode_actions = [
     # disable keyboard backlight brightness
     "echo 0 | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
     # save capslock led brightness (do a backup)
-    "cat /sys/class/leds/input3::capslock/brightness | sudo tee /tmp/input3_capslock_brightness",
+    "cat /sys/class/leds/input2::capslock/brightness | sudo tee /tmp/input2_capslock_brightness",
     # disable capslock led brightness
-    "echo 0 | sudo tee /sys/class/leds/input3::capslock/brightness",
+    "echo 0 | sudo tee /sys/class/leds/input2::capslock/brightness",
     # save micmute led brightness (do a backup)
     "cat /sys/class/leds/platform::micmute/brightness | sudo tee /tmp/micmute_brightness",
     # disable micmute led brightness
     "echo 0 | sudo tee /sys/class/leds/platform::micmute/brightness",
+    # save camera led brightness (do a backup)
+    "echo '0x60079' | sudo tee '/sys/kernel/debug/asus-nb-wmi/dev_id' >/dev/null && VALUE=$(sudo cat '/sys/kernel/debug/asus-nb-wmi/dsts' | rev | cut -d' ' -f1 | rev) && echo -n $VALUE | tail -c 1 | sudo tee /tmp/camera_brightness",
+    # disable camera led brightness
+    "echo '0x60079' | sudo tee '/sys/kernel/debug/asus-nb-wmi/dev_id' >/dev/null && echo '0' | sudo tee '/sys/kernel/debug/asus-nb-wmi/ctrl_param' && sudo cat '/sys/kernel/debug/asus-nb-wmi/devs' >/dev/null"
 ]
 
 flip_key = EV_KEY.KEY_PROG2
