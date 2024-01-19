9bf2d0126781cce344e54745cb25fb8faec20d74
Lukáš Drahník <ldrahnik@gmail.com>
2024-01-19 23:30:47 +0100

    Fixed changing number /sys/class/leds/input<number> by wildcard

--- conf/default.py
+++ conf/default.py
@@ -4,7 +4,7 @@ laptop_mode_actions = [
     # setting up saved keyboard backlight brightness (restore from backup)
     "test -f /tmp/kbd_backlight_brightness && cat /tmp/kbd_backlight_brightness | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
     # setting up saved capslock backlight brightness (restore from backup)
-    "test -f /tmp/input2_capslock_brightness && cat /tmp/input2_capslock_brightness | sudo tee /sys/class/leds/input2::capslock/brightness",
+    "test -f /tmp/input_capslock_brightness && cat /tmp/input_capslock_brightness | sudo tee /sys/class/leds/input*::capslock/brightness",
     # setting up saved micmute backlight brightness (restore from backup)
     "test -f /tmp/micmute_brightness && cat /tmp/micmute_brightness | sudo tee /sys/class/leds/platform::micmute/brightness",
     # setting up saved camera backlight brightness (restore from backup)
@@ -17,9 +17,9 @@ tablet_mode_actions = [
     # disable keyboard backlight brightness
     "echo 0 | sudo tee /sys/class/leds/asus::kbd_backlight/brightness",
     # save capslock led brightness (do a backup)
-    "cat /sys/class/leds/input2::capslock/brightness | sudo tee /tmp/input2_capslock_brightness",
+    "cat /sys/class/leds/input*::capslock/brightness | head -n 1 | sudo tee /tmp/input_capslock_brightness",
     # disable capslock led brightness
-    "echo 0 | sudo tee /sys/class/leds/input2::capslock/brightness",
+    "echo 0 | sudo tee /sys/class/leds/input*::capslock/brightness",
     # save micmute led brightness (do a backup)
     "cat /sys/class/leds/platform::micmute/brightness | sudo tee /tmp/micmute_brightness",
     # disable micmute led brightness
